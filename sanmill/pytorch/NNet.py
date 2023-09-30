5797a47a0be328f111de32a03716ad0ab7343f84
cheanus <lin.pyw@qq.com>
2023-10-01 00:15:56 +0800

    feat: implemented on sanmill

---
+++ sanmill/pytorch/NNet.py
@@ -0,0 +1,134 @@
+import os
+import sys
+import time
+
+import numpy as np
+from tqdm import tqdm
+
+sys.path.append('../../')
+from utils import *
+from NeuralNet import NeuralNet
+
+import torch
+import torch.optim as optim
+
+from .SanmillNNet import SanmillNNet as snnet
+
+args = dotdict({
+    'lr': 0.001,
+    'dropout': 0.5,
+    'epochs': 10,
+    'batch_size': 64,
+    'cuda': torch.cuda.is_available(),
+    'num_channels': 512,
+})
+
+
+class NNetWrapper(NeuralNet):
+    def __init__(self, game):
+        self.nnet = snnet(game, args)
+        self.board_x, self.board_y = game.getBoardSize()
+
+        if args.cuda:
+            self.nnet.cuda()
+
+    def train(self, examples):
+        """
+        examples: list of examples, each example is of form (board, pi, v, period)
+        """
+        optimizer = optim.Adam(self.nnet.parameters(), lr=args.lr)
+
+        for epoch in range(args.epochs):
+            print('EPOCH ::: ' + str(epoch + 1))
+            self.nnet.train()
+            pi_losses = AverageMeter()
+            v_losses = AverageMeter()
+
+            batch_count = int(len(examples) / args.batch_size)
+
+            t = tqdm(range(batch_count), desc='Training Net')
+            for _ in t:
+                sample_ids = np.random.randint(len(examples), size=args.batch_size)
+                boards, pis, vs, periods = list(zip(*[examples[i] for i in sample_ids]))
+                boards = torch.tensor(boards, dtype=torch.float32)
+                target_pis = torch.tensor(pis, dtype=torch.float32)
+                target_vs = torch.tensor(vs, dtype=torch.float32)
+                periods = torch.tensor(periods, dtype=torch.int8)
+
+                # predict
+                if args.cuda:
+                    # boards, target_pis, target_vs = boards.contiguous().cuda(), target_pis.contiguous().cuda(), target_vs.contiguous().cuda()
+                    boards = boards.contiguous().cuda()
+                    target_pis = target_pis.contiguous().cuda()
+                    target_vs = target_vs.contiguous().cuda()
+                    periods = periods.contiguous().cuda()
+
+                # compute output
+                out_pi = torch.zeros(target_pis.size()).cuda()
+                out_v = torch.zeros(target_vs.size()).cuda()
+                for i in range(4):
+                    pi, v = self.nnet(boards[periods==i], i)
+                    out_pi[periods==i] = pi.view(-1, target_pis.size(1))
+                    out_v[periods==i] = v.view(-1)
+                l_pi = self.loss_pi(target_pis, out_pi, periods)
+                l_v = self.loss_v(target_vs, out_v)
+                total_loss = l_pi + l_v
+
+                # record loss
+                pi_losses.update(l_pi.item(), boards.size(0))
+                v_losses.update(l_v.item(), boards.size(0))
+                t.set_postfix(Loss_pi=pi_losses, Loss_v=v_losses)
+
+                # compute gradient and do SGD step
+                optimizer.zero_grad()
+                total_loss.backward()
+                optimizer.step()
+
+    def predict(self, board, period):
+        """
+        board: np array with board
+        """
+        # timing
+        start = time.time()
+
+        # preparing input
+        board = torch.tensor(board, dtype=torch.float)
+        period = torch.tensor(period, dtype=torch.int8)
+        if args.cuda: board = board.contiguous().cuda()
+        board = board.view(1, self.board_x, self.board_y)
+        self.nnet.eval()
+        with torch.no_grad():
+            pi, v = self.nnet(board, period)
+
+        # print('PREDICTION TIME TAKEN : {0:03f}'.format(time.time()-start))
+        return torch.exp(pi).data.cpu().numpy()[0], v.data.cpu().numpy()[0]
+
+    def loss_pi(self, targets, outputs, periods):
+        # mask = torch.ones_like(targets)
+        # mask[(periods==0)|(periods==3), 24:-1] = 0
+        return -torch.sum(targets * outputs) / targets.size()[0]
+
+    def loss_v(self, targets, outputs):
+        return torch.sum((targets - outputs.view(-1)) ** 2) / targets.size()[0]
+
+    def save_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
+        filepath = os.path.join(folder, filename)
+        if not os.path.exists(folder):
+            print("Checkpoint Directory does not exist! Making directory {}".format(folder))
+            os.mkdir(folder)
+        else:
+            print("Checkpoint Directory exists! ")
+        torch.save({
+            'state_dict': self.nnet.state_dict(),
+        }, filepath)
+
+    def load_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
+        # https://github.com/pytorch/examples/blob/master/imagenet/main.py#L98
+        filepath = os.path.join(folder, filename)
+        if not os.path.exists(filepath):
+            # raise ("No model in path {}".format(filepath))
+            print("No model in path {}".format(filepath))
+            exit(1)
+        map_location = None if args.cuda else 'cpu'
+        checkpoint = torch.load(filepath, map_location=map_location)
+        self.nnet.load_state_dict(checkpoint['state_dict'])
---
+++ sanmill/pytorch/SanmillNNet.py
@@ -0,0 +1,71 @@
+import sys
+sys.path.append('..')
+from utils import *
+
+import torch.nn as nn
+
+class Branch(nn.Module):
+    def __init__(self, args):
+        super(Branch, self).__init__()
+        self.main = nn.Sequential(
+            nn.Linear(512*9, args.num_channels*2),
+            nn.LayerNorm(args.num_channels*2),
+            nn.ReLU(),
+            nn.Dropout(args.dropout),
+            nn.Linear(args.num_channels*2, args.num_channels),
+            nn.LayerNorm(args.num_channels),
+            nn.ReLU(),
+            nn.Dropout(args.dropout),
+        )
+        self.pi = nn.Sequential(
+            nn.Linear(args.num_channels, 24*24+1),
+            nn.LogSoftmax(dim=1),
+        )
+        self.v = nn.Sequential(
+            nn.Linear(args.num_channels, 1),
+            nn.Tanh(),
+        )
+
+    def forward(self, s):
+        """
+        s: batch_size x 512*9
+        """
+        s = self.main(s)  # batch_size x num_channels
+        pi = self.pi(s)  # batch_size x 24*24+1
+        v = self.v(s)
+        return pi, v
+
+
+class SanmillNNet(nn.Module):
+    def __init__(self, game, args):
+        # game params
+        self.board_x, self.board_y = game.getBoardSize()
+        self.args = args
+
+        super(SanmillNNet, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(1, args.num_channels//4, 3, stride=1, padding=1),
+            nn.InstanceNorm2d(args.num_channels//4),
+            nn.ReLU(),
+            nn.Conv2d(args.num_channels//4, args.num_channels//4, 3, stride=1, padding=1),
+            nn.InstanceNorm2d(args.num_channels//4),
+            nn.ReLU(),
+            nn.Conv2d(args.num_channels//4, args.num_channels//2, 3, stride=1),
+            nn.InstanceNorm2d(args.num_channels//2),
+            nn.ReLU(),
+            nn.Conv2d(args.num_channels//2, args.num_channels, 3, stride=1),
+            nn.InstanceNorm2d(args.num_channels),
+            nn.ReLU(),
+        )
+        self.branch = nn.ModuleList([Branch(args) for _ in range(4)])
+
+    def forward(self, s, period):
+        """
+        s: batch_size x board_x x board_y
+        period: 1
+        """
+        s = s.view(-1, 1, self.board_x, self.board_y)  # batch_size x 1 x (board_x-4) x (board_y-4)
+        s = self.main(s)  # batch_size x num_channels x (board_x-4) x (board_y-4)
+        s = s.view(-1, self.args.num_channels*(self.board_x-4)*(self.board_y-4))  # batch_size x num_channels*(board_x-4)*(board_y-4)
+        pi, v = self.branch[period](s)
+        return pi, v
