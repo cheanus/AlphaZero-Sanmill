736fbd163af5294e106cbe563f6deab85e6fbd9a
cheanus <lin.pyw@qq.com>
2023-10-28 23:58:25 +0800

    feat: use ViT-like backbone

--- sanmill/pytorch/SanmillNNet.py
+++ sanmill/pytorch/SanmillNNet.py
@@ -11,16 +11,15 @@ class Branch03(nn.Module):
         super(Branch03, self).__init__()
         self.args = args
         self.main = nn.Sequential(
-            nn.LayerNorm(512*9),
+            nn.LayerNorm(args.num_channels),
             nn.ReLU(),
-            nn.Linear(512*9, args.num_channels*2),
+            nn.Linear(args.num_channels, args.num_channels*2),
             nn.Dropout(args.dropout),
             nn.LayerNorm(args.num_channels*2),
             nn.ReLU(),
             nn.Linear(args.num_channels*2, args.num_channels),
             nn.Dropout(args.dropout),
         )
-        self.main_identity = nn.Linear(512*9, args.num_channels)
         self.pi = nn.Sequential(
             nn.ReLU(),
             nn.Linear(args.num_channels, 24),
@@ -38,7 +37,7 @@ class Branch03(nn.Module):
         """
         s: batch_size x 512*9
         """
-        s = self.main(s) + self.main_identity(s)  # batch_size x num_channels
+        s = self.main(s) + s  # batch_size x num_channels
         pi = torch.ones((s.size()[0], 24*24)).to(device) * -10
         pi[:,:24] = self.pi(s)  # batch_size x 24
         v = self.v(s)  # batch_size x 1
@@ -53,16 +52,15 @@ class Branch14(nn.Module):
         self.b = Board()
         self.cache_valids()
         self.main = nn.Sequential(
-            nn.LayerNorm(512*9),
+            nn.LayerNorm(args.num_channels),
             nn.ReLU(),
-            nn.Linear(512*9, args.num_channels*2),
+            nn.Linear(args.num_channels, args.num_channels*2),
             nn.Dropout(args.dropout),
             nn.LayerNorm(args.num_channels*2),
             nn.ReLU(),
             nn.Linear(args.num_channels*2, args.num_channels),
             nn.Dropout(args.dropout),
         )
-        self.main_identity = nn.Linear(512*9, args.num_channels)
         self.pi = nn.Sequential(
             nn.ReLU(),
             nn.Linear(args.num_channels, 80),
@@ -87,7 +85,7 @@ class Branch14(nn.Module):
         """
         s: batch_size x 512*9
         """
-        s = self.main(s) + self.main_identity(s)  # batch_size x num_channels
+        s = self.main(s) + s  # batch_size x num_channels
         pi = torch.ones((s.size()[0], 24*24),dtype=torch.float).to(device) * -10
         pi[:, self.valids] = self.pi(s)
         v = self.v(s)  # batch_size x 1
@@ -97,16 +95,15 @@ class Branch2(nn.Module):
     def __init__(self, args):
         super(Branch2, self).__init__()
         self.main = nn.Sequential(
-            nn.LayerNorm(512*9),
+            nn.LayerNorm(args.num_channels),
             nn.ReLU(),
-            nn.Linear(512*9, args.num_channels*2),
+            nn.Linear(args.num_channels, args.num_channels*2),
             nn.Dropout(args.dropout),
             nn.LayerNorm(args.num_channels*2),
             nn.ReLU(),
             nn.Linear(args.num_channels*2, args.num_channels),
             nn.Dropout(args.dropout),
         )
-        self.main_identity = nn.Linear(512*9, args.num_channels)
         self.pi = nn.Sequential(
             nn.ReLU(),
             nn.Linear(args.num_channels, 24*24),
@@ -123,7 +120,7 @@ class Branch2(nn.Module):
         """
         s: batch_size x 512*9
         """
-        s = self.main(s) + self.main_identity(s)  # batch_size x num_channels
+        s = self.main(s) + s  # batch_size x num_channels
         pi = self.pi(s)  # batch_size x 24*24
         v = self.v(s)  # batch_size x 1
         return pi, v
@@ -136,18 +133,14 @@ class SanmillNNet(nn.Module):
 
         super(SanmillNNet, self).__init__()
         self.main = nn.Sequential(
-            nn.Conv2d(1, args.num_channels//4, 3, stride=1, padding=1),
-            nn.BatchNorm2d(args.num_channels//4),
-            nn.ReLU(),
-            nn.Conv2d(args.num_channels//4, args.num_channels//2, 3, stride=1),
-            nn.BatchNorm2d(args.num_channels//2),
-            nn.ReLU(),
-            nn.Conv2d(args.num_channels//2, args.num_channels, 3, stride=1),
+            nn.Conv2d(1, args.num_channels, 3, stride=1, padding=1),
             nn.BatchNorm2d(args.num_channels),
             nn.ReLU(),
         )
-        self.branch = nn.ModuleList([Branch03(args), Branch14(args), Branch2(args), Branch03(args), Branch14(args)])
+        self.attension_b = nn.Parameter(torch.rand(self.board_x*self.board_y, args.num_channels))
         self.attension = nn.MultiheadAttention(args.num_channels, 4, batch_first=True)
+        self.mlp = nn.Linear(self.args.num_channels*self.board_x*self.board_y, self.args.num_channels)
+        self.branch = nn.ModuleList([Branch03(args), Branch14(args), Branch2(args), Branch03(args), Branch14(args)])
 
     def forward(self, s, period):
         """
@@ -155,9 +148,11 @@ class SanmillNNet(nn.Module):
         period: 1
         """
         s = s.view(-1, 1, self.board_x, self.board_y)  # batch_size x 1 x board_x x board_y
-        s = self.main(s)  # batch_size x num_channels x (board_x-4) x (board_y-4)
-        s = s.view(-1, self.args.num_channels, (self.board_x-4)*(self.board_y-4)).permute(0,2,1)  # batch_size x (board_x-4)*(board_y-4) x num_channels
-        s, _ = self.attension(s, s, s)  # batch_size x (board_x-4)*(board_y-4) x num_channels
-        s = s.reshape(-1, self.args.num_channels*(self.board_x-4)*(self.board_y-4))  # batch_size x num_channels*(board_x-4)*(board_y-4)
+        s = self.main(s)  # batch_size x num_channels x board_x x board_y
+        s = s.view(-1, self.args.num_channels, self.board_x*self.board_y).permute(0,2,1)  # batch_size x board_x*board_y x num_channels
+        s = s + self.attension_b  # batch_size x board_x*board_y x num_channels
+        s, w = self.attension(s, s, s)  # batch_size x board_x*board_y x num_channels
+        s = s.reshape(-1, self.args.num_channels*self.board_x*self.board_y)  # batch_size x num_channels*board_x*board_y
+        s = self.mlp(s)  # batch_size x num_channels
         pi, v = self.branch[period](s)
         return pi, v
